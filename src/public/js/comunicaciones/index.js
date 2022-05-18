365b6ebc83fe964eaac2e74024ed9e0e5ca5e150
kenay <omarlazor@hotmail.com>
2022-05-18 11:25:26 -0500

    finishes maybe

--- src/public/js/comunicaciones/index.js
+++ src/public/js/comunicaciones/index.js
@@ -14,8 +14,18 @@ form.addEventListener('submit', (e) => {
             }if(e.cuenta) {
                 document.getElementById('qr').setAttribute('src','img/'+e.cuenta+".svg");
             }
+            window.location.href="#popup1"
         },
         error: (e) => {
             console.log("server can't give one response");
-        }});
+        }
     });
+});
+
+document.addEventListener('keydown',(e) => {
+    if (event.ctrlKey) {
+        if (event.keyCode == 82 || event.keyCode == 116) {
+            window.location.href = "/"
+        }
+     }
+})
